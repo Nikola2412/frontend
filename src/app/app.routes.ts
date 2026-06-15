e58a356e2d57fbc2eefdb0323c1c0fdf35bf2aa9
Nikola2412 <83644688+Nikola2412@users.noreply.github.com>
2026-06-15 20:51:31 +0200

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Frontpage } from './frontpage/frontpage';
+import { Loginpage } from './loginpage/loginpage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:Frontpage},
+    {path:"login",component:Loginpage}
+];
---
+++ src/app/loginpage/loginpage.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginpage',
+  imports: [],
+  templateUrl: './loginpage.html',
+  styleUrl: './loginpage.css',
+})
+export class Loginpage{
+}
