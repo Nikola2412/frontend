462b3ae0ac3e6a1692ee9a3edaac25dba0c429f8
Nikola2412 <83644688+Nikola2412@users.noreply.github.com>
2026-06-19 21:46:44 +0200

    promotions and facility

--- src/app/promotion/promotion.ts
+++ src/app/promotion/promotion.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Promotion } from '../models/promotion';
 import { PromotionService } from '../services/promotion-service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-promotion',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './promotion.html',
   styleUrl: './promotion.css',
 })
